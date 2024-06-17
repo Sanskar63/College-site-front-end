8bb7d2aed56836546604ceee53a6b654859af57b
Sanskar <sanskar06march@gmail.com>
2024-06-17 12:42:32 +0530

    url changed to render

--- src/components/utils/constant.jsx
+++ src/components/utils/constant.jsx
@@ -1,3 +1,3 @@
-export const url = 'http://localhost:8000/api/v1';
+// export const url = 'http://localhost:8000/api/v1';
 
-// export const url = 'https://college-site.onrender.com/api/v1';
+export const url = 'https://college-site.onrender.com/api/v1';
